e6bc4d31c6f1e9c42e1ecdffa9fe537a10e42cc7
Vincent Peltier <vincentpeltier.pro@outlook.fr>
2025-09-25 18:05:15 +0200

    reset base, recréation migration suite à une erreur, test ci 25/09/2025 : 18h05

--- src/DataFixtures/ChangelogFixtures.php
+++ src/DataFixtures/ChangelogFixtures.php
@@ -5,16 +5,20 @@ namespace App\DataFixtures;
 use App\Entity\Changelog;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Cocur\Slugify\Slugify;
 
 class ChangelogFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $slugify = new Slugify();
+
         $changelog = new Changelog();
-        $changelog->setVersion('1.6');
+        $changelog->setVersion('1.0');
         $changelog->setDate(new \DateTime('2025-09-07'));
         $changelog->setContent("Ajout : système de craft\nCorrection : bug d'affichage dans l'inventaire");
         $changelog->setImage('changelog_01.png');
+        $changelog->setSlug($slugify->slugify('Version 1.0'));
 
         $manager->persist($changelog);
         $manager->flush();
